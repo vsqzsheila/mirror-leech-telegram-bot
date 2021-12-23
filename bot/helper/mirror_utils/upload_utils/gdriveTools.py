abefc74d25ebfbd50d6a00401cdf2a49eb38e7db
vsqzsheila <94713655+vsqzsheila@users.noreply.github.com>
2021-12-23 23:32:08 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -168,7 +168,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Xiao Uploaded',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -452,7 +452,7 @@ class GoogleDriveHelper:
     def create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Xiao Uploaded",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
